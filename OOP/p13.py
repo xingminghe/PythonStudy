9ca6b458025822ff6674f1b64eb235feaeaf83a6
xingming.he <xingming.he@fih-foxconn.com>
2019-09-04 16:32:27 +0800

    高阶函数

--- OOP/p13.py
+++ OOP/p13.py
@@ -6,3 +6,4 @@ logging.info("This is a info log")
 logging.warning("This is a warning log")
 logging.error("This is a error log")
 logging.critical("This is a critical log")
+s = ["Neo","Matrix","Xirtam","Oen"]
